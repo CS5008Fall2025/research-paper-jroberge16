fee5671ba19d839fb4d0cad79e03e413f4e10905
jroberge16 <jwr1015@outlook.com>
2025-11-30 19:57:48 -0500

    comments and clean up

--- trees/benchmarking/bench_utils.c
+++ trees/benchmarking/bench_utils.c
@@ -11,7 +11,11 @@
 
 typedef void (*AVL_OP_FUNC)(AVLIndex* tree, int value);
 
-
+/**
+ * Retrieves the next number in a file
+ * @param file file pointer
+ * @return next integer from file, or -1 if EOF
+ */
 int get_next_number(FILE* file){
     char buffer[LINE_BUFFER_SIZE];
     if(fgets(buffer, LINE_BUFFER_SIZE, file) != NULL){
@@ -23,6 +27,11 @@ int get_next_number(FILE* file){
 }
 
 
+/**
+ * Appends to a CSV file
+ * @param lines array of strings to write
+ * @param file_name name of the file to write to
+ */
 void append_lines_to_file(char** lines, char* file_name){
         FILE* fptr = fopen(file_name, "w");
         if(fptr == NULL){
@@ -34,7 +43,11 @@ void append_lines_to_file(char** lines, char* file_name){
         }
         fclose(fptr);
 }
-
+/**
+ * Frees the results array
+ * @param results array of strings
+ * @param count number of strings
+ */
 void free_results(char** results, int count){
     for(int i = 0; i < count; i++){ 
         if(results[i] != NULL){
@@ -44,17 +57,29 @@ void free_results(char** results, int count){
     free(results);
 }
 
+/**
+ * Assign CVS HEADEr to string array
+ */
 void assign_header(char* header_string, char** results){
     char buffer[LINE_BUFFER_SIZE];
     snprintf(buffer, sizeof(buffer), "%s", header_string);
     results[0] = strdup(buffer);
 }
 
+/**
+ * SKip the header of the CSV file
+ * @param file file pointer
+ */
 void skip_header(FILE* file){
     char header[LINE_BUFFER_SIZE];
     fgets(header, LINE_BUFFER_SIZE, file);
 }
 
+/**
+ * Random number generator in range [min, max]
+ * @param min minimum value
+ * @param max maximum value
+ */
 int getRandomInRange(int min, int max) {
     return (rand() % (max - min + 1)) + min;
 }
